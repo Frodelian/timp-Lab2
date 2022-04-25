1389a347cd95f319caaa1984956091d45d9026cf
Frodelian <91691109+Frodelian@users.noreply.github.com>
2022-04-26 01:26:02 +0300

    Update Hello_world.cpp

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,9 +1,8 @@
-edit "Hello_world.cpp"
 #include <iostream>
 #include <string>
  
 int main(int argc, char** argv){
- string name; // User name
- std::cin >> name; // Input user name
+ string name; // User n.
+ std::cin >> name; // Ввод данных
  std::cout << "Hello world from " << name << std::endl;
-} 
+}  
